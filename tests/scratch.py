96e23535bef75cf9e24ae5340674a52dedb49fb5
Carlos <carlosavv@utexas.edu>
2020-08-29 19:56:57 -0500

    wip

--- tests/scratch.py
+++ tests/scratch.py
@@ -16,18 +16,19 @@ def main():
 	checkPrice = True
 	currency = 'BTC-USD'
 
-
 	# Granularity (in seconds). So 300 = data from every 5 min (300s/60s = 5 mins)
 	period = 300
-	pub_client = cbpro.PublicClient()
 	auth_client = cbpro.AuthenticatedClient(key,secret,passphrase)
 	# Get the currency's specific ID
 	# specificID = get_SpecificAccount(auth_client,currency[:3])
-	print('hello')
+	# currentPrice = []
 	while checkPrice == True:
-		currentPrice = get_CurrentPrice(auth_client,currency,period)
+		currentPrice,price_mat = get_CurrentPrice(auth_client,currency,period)
 		time.sleep(1)
-		print(currentPrice)
+		print(price_mat)
+		
+	
+		
 main()
 	# Wait for 1 second, to avoid API limit
 	# time.sleep(1)
@@ -44,33 +45,6 @@ main()
 # There are 3 different commonly used methods for the exact calculation of AvgU and AvgD (see details below)
 
 
-# def RSIfun(price, n=14):
-#     delta = price['Close'].diff()
-#     #-----------
-#     dUp=
-#     dDown=
-
-#     RolUp=pd.rolling_mean(dUp, n)
-#     RolDown=pd.rolling_mean(dDown, n).abs()
-
-#     RS = RolUp / RolDown
-#     rsi= 100.0 - (100.0 / (1.0 + RS))
-	# dUp= delta[delta > 0]
-	# dDown= delta[delta < 0]
-
-	# dUp = dUp.reindex_like(delta, fill_value=0)
-	# dDown = dDown.reindex_like(delta, fill_value=0)
-
-	# dUp, dDown = delta.copy(), delta.copy()
-	# dUp[dUp < 0] = 0
-	# dDown[dDown > 0] = 0
-
-	# RolUp = pd.rolling_mean(dUp, n)
-	# RolDown = pd.rolling_mean(dDown, n).abs()
-
-	# RS = RolUp / RolDown
-
-#     return rsi
 
 # calculate 
 
--- tests/tools_test.py
+++ tests/tools_test.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import time
 
 def get_SpecificAccount(auth_client,cur):
     x = auth_client.get_accounts()
@@ -9,13 +10,13 @@ def get_SpecificAccount(auth_client,cur):
 def get_CurrentPrice(auth_client,currency,period):
 
 	historicData = auth_client.get_product_historic_rates(currency, granularity=period)
-
+	
 	# Make an array of the historic price data from the matrix
 	price_array = np.squeeze(np.asarray(np.matrix(historicData)[:,4]))
 
 	# Get latest data and show to the user for reference
-	newData = auth_client.get_product_ticker(product_id=currency)
-	# print(newData)
-	currentPrice=newData['price']
+	latestData = auth_client.get_product_ticker(product_id=currency)
+	
+	currentPrice = latestData['price']
 	# print(str(currency) + " price =",currentPrice)
-	return float(currentPrice)
+	return float(currentPrice),price_array
