5d43367ee3013f3761256ffa92916da2ef678d6a
Carlos Villarreal <carlosavv@utexas.edu>
2020-09-01 11:03:54 -0500

    wip

---
+++ tests/indicators_test.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+def computeRSI(price_data,N = 14):
+	'''
+	RSI - relative strength index: momentum oscillator that predicts oversold/overbough conditions
+	
+	N - window of closing price (default is 14)
+	price_data - array with closing price data
+
+	'''
+	
