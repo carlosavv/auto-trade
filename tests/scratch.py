f6245c15de9526896fdb8065c92e4538969f3819
Carlos <carlosavv@utexas.edu>
2020-11-03 21:03:01 -0600

    wip

--- tests/scratch.py
+++ tests/scratch.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time 
 import datetime as dt
 import numpy as np
-from tools_test import get_CurrentPrice
+from tools_test import get_CurrentPrice,get_SpecificAccount
 
 def main():
 
@@ -17,21 +17,69 @@ def main():
 	currency = 'BTC-USD'
 
 	# Granularity (in seconds). So 300 = data from every 5 min (300s/60s = 5 mins)
-	period = 300
+	period = 60
 	auth_client = cbpro.AuthenticatedClient(key,secret,passphrase)
 	# Get the currency's specific ID
-	# specificID = get_SpecificAccount(auth_client,currency[:3])
+	specificID = get_SpecificAccount(auth_client,currency[:3])
+	print(specificID)
 	# currentPrice = []
 	j = 0
-	while j < 3:
-		currentPrice,price_mat = get_CurrentPrice(auth_client,currency,period)
+	while j < 500:
+		currentPrice,closing_price_array = get_CurrentPrice(auth_client,currency,period)
 		time.sleep(1)
-		print(price_mat)
-		# for i in range(0,len(price_mat)):
-		# 	print(price_mat[i][4])
+		avg_closing_price = []
+		avg_closing_price.append(np.mean(closing_price_array))
+		upPrices = []
+		downPrices = []
+		i = 0
+		while i < len(avg_closing_price):
+			if i == 0:
+				upPrices.append(0)
+				downPrices.append(0)
+			else:
+				if (avg_closing_price[i]-avg_closing_price[i-1] ) > 0:
+					upPrices.append(avg_closing_price[i]-avg_closing_price[i-1])
+					downPrices.append(0)
+				else:
+					downPrices.append(avg_closing_price[i]-avg_closing_price[i-1])
+					upPrices.append(0)
+			i += 1
+		x = 0
+		avg_gain = []
+		avg_loss = []
+		#  Loop to calculate the average gain and loss
+		while x < len(upPrices):
+			if x <15:
+				avg_gain.append(0)
+				avg_loss.append(0)
+			else:
+				sumGain = 0
+				sumLoss = 0
+				y = x-14
+				while y<=x:
+					sumGain += upPrices[y]
+					sumLoss += downPrices[y]
+					y += 1
+				avg_gain.append(sumGain/14)
+				avg_loss.append(abs(sumLoss/14))
+			x += 1
+		p = 0
+		RS = []
+		RSI = []
+		#  Loop to calculate RSI and RS
+		while p < len(avg_closing_price):
+			if p <15:
+				RS.append(0)
+				RSI.append(0)
+			else:
+				RSvalue = (avg_gain[p]/avg_loss[p])
+				RS.append(RSvalue)
+				RSI.append(100 - (100/(1+RSvalue)))
+			p+=1
 		j += 1
-	
-		
+	print(avg_closing_price)
+	print(RSI)	
+
 main()
 	# Wait for 1 second, to avoid API limit
 	# time.sleep(1)
--- tests/tools_test.py
+++ tests/tools_test.py
@@ -2,19 +2,29 @@ import numpy as np
 import time
 
 def get_SpecificAccount(auth_client,cur):
-    x = auth_client.get_accounts()
-    for account in x:
-        if account['currency'] == cur:
-            return account['id']
+	'''
+	params: auth_client - coinbase API client
+
+	returns: ID of specific currency
+
+	'''
+	x = auth_client.get_accounts()
+	for account in x:
+		if account['currency'] == cur:
+			return account['id']
 
 def get_CurrentPrice(auth_client,currency,period):
 
 	historicData = auth_client.get_product_historic_rates(currency, granularity=period)
 	# print(historicData)
 
+	# store an array with the closing array
+	closing_price_array = np.squeeze(np.asarray(np.matrix(historicData)[:,4]))
+
+	
 	# Get latest data and show to the user for reference
 	latestData = auth_client.get_product_ticker(product_id=currency)
-	
+	# print(latestData)
 	currentPrice = latestData['price']
 	# print(str(currency) + " price =",currentPrice)
-	return float(currentPrice),historicData
+	return float(currentPrice),closing_price_array
