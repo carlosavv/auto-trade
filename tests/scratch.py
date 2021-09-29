0e882f16d80b9ea1edeabda4c994912fcdc2d2e8
Carlos <carlosavv@utexas.edu>
2021-09-29 12:29:35 -0500

    wip

--- tests/scratch.py
+++ tests/scratch.py
@@ -1,59 +1,154 @@
+from logging import debug
 from WebsocketClient import WebsocketFeed
-import plotly
 import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Output, Input
 import plotly.graph_objects as go
 from collections import deque
-import random
+import websocket
+import json
+import dateutil.parser
+import time
+import _thread
 
 
-class Visualizer(WebsocketFeed):
-    def __init__(self, url, request):
-        WebsocketFeed.__init__(self, url, request)
+class Visualizer(object):
+    def __init__(self, time, price):
 
+        self.time = time
+        self.closingPrice = price
         self.app = dash.Dash(__name__)
-        self.tickerTime = deque(maxlen=200)
-        self.closingPrice = deque(maxlen=200)
-        print(self.get_time())
+
         self.app.layout = html.Div(
             [
-                dcc.Graph(id = 'live-graph', animate = True),
-                dcc.Interval(
-                    id = 'graph-update',
-                    interval = 1000,
-                    n_intervals = 0
-                )
+                dcc.Graph(id="live-graph", animate=True),
+                dcc.Interval(id="graph-update", interval=1000, n_intervals=0),
             ]
         )
         self.app.callback(
-            Output('live-graph', 'figure'),
-            [Input('graph-update', 'n_intervals')])(self.update_graph)
-        self.app.run_server(debug=True)
-    
-    def update_time(self):
-        self.tickerTime.append(self.get_time())
-        return self.tickerTime
-    
-    def update_price(self):
-        self.closingPrice.append(self.get_closingPrice())
-        return self.closingPrice
+            Output("live-graph", "figure"), [Input("graph-update", "n_intervals")]
+        )(self.update_graph)
 
     def update_graph(self, n):
 
         data = go.Scatter(
-            x = list(self.update_time()),
-            y = list(self.update_price()),
-            name ='Scatter',
-            mode = 'lines+markers'
+            x=list(self.time),
+            y=list(self.closingPrice),
+            name="Scatter",
+            mode="lines+markers",
         )
 
+        return {
+            "data": [data],
+            "layout": go.Layout(
+                xaxis=dict(range=[min(self.time), max(self.time)]),
+                yaxis=dict(range=[min(self.closingPrice), max(self.closingPrice)]),
+            ),
+        }
 
-        return {'data': [data],
-                'layout': go.Layout(xaxis=dict(range=[min(self.update_time()), max(self.update_time())]),
-                yaxis=dict(range=[min(self.update_price()), max(self.update_price())]),)}
 
+class Tick(object):
+    def __init__(self):
+        self.current = None
+        self.previous = None
+
+
+class WebsocketFeed(object):
+    def __init__(self, url, request):
+        self.url = url
+        self.timeProcessed = {}
+        self.candlesticks = []
+        self.request = request
+        self.timestamp = None
+        self.tickerTime = deque(maxlen=200)
+        self.closingPrice = deque(maxlen=200)
+
+        self.ws = websocket.WebSocketApp(
+            self.url, on_open=self.on_open, on_message=self.on_message
+        )
+
+        self.ws.run_forever()
+
+    def on_open(self, ws):
+        print("=== Websocket is now open! ===")
+
+        def run(*args):
+            # count = 0
+            # while True:
+            time.sleep(1)
+            # self.ws.send("Hello %d" % count)
+            self.ws.send(json.dumps(self.request))
+            # count += 1
+
+        _thread.start_new_thread(run, ())
+
+    def on_message(self, ws, message):
+        tick = Tick()
+        tick.current = json.loads(message)
+        tick.previous = tick.current
+
+        tick_time = dateutil.parser.parse(tick.current["time"])
+        self.timestamp = tick_time.strftime("%m/%d/%Y %H:%M")
+
+        if self.timestamp not in self.timeProcessed:
+            # print("Starting new candlestick")
+            self.timeProcessed[self.timestamp] = True
+            if len(self.candlesticks) > 0:
+                self.candlesticks[-1]["close"] = tick.previous["price"]
+                print(self.candlesticks)
+                # TODO: try to get the last close and time from the candlesticks array
+
+                # for dict in self.candlesticks:
+
+                #     self.closingPrice.append(dict["close"])
+                #     print(dict["close"])
+                #     self.tickerTime.append(
+                #         int(list(self.timeProcessed.keys())[-1][14:16])
+                #     )
+                #     print(int(list(self.timeProcessed.keys())[0][14:16]))
+
+            self.candlesticks.append(
+                {
+                    "time": self.timestamp,
+                    "open": tick.current["price"],
+                    "high": tick.current["price"],
+                    "low": tick.current["price"],
+                }
+            )
+
+        if len(self.candlesticks) > 0:
+            next_tick = self.candlesticks[-1]
+
+            if tick.current["price"] > next_tick["high"]:
+                next_tick["high"] = tick.current["price"]
+
+            elif tick.current["price"] < next_tick["low"]:
+                next_tick["low"] = tick.current["price"]
+
+        # vis = Visualizer(self.update_time(), self.update_price())
+        # vis.app.run_server(debug=True)
+
+    def get_candlesticks(self):
+        return self.candlesticks
+
+    # def get_closingPrice(self):
+
+    #     for dict in self.candlesticks:
+    #         if "close" in list(dict.keys()):
+
+    #             return dict["close"]
+
+    def get_time(self):
+        return int(list(self.timeProcessed.keys())[-1][14:16])
+
+    def update_time(self):
+        self.tickerTime.append(self.get_time())
+        return self.tickerTime
+
+    def update_price(self):
+        self.closingPrice.append(self.get_closingPrice())
+        return self.closingPrice
 
 
 socket = "wss://ws-feed.pro.coinbase.com"
@@ -63,7 +158,7 @@ subscribeMessage = {
     "channels": [{"name": "ticker", "product_ids": ["BTC-USD"]}],
 }
 
-stream = Visualizer(socket, subscribeMessage)
+stream = WebsocketFeed(socket, subscribeMessage)
 print("")
 
 
